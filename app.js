728f01648ce719e7ca92f02324543e332f2af29d
Abdulmalik <umarusmonov0203@gmail.com>
2020-09-19 12:26:46 +0500

    set up new things

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.APIKEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -22,7 +20,6 @@ const userScheme = new mongoose.Schema ({
     password: String
 });
 
-userScheme.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 const User = new mongoose.model("User", userScheme);
 
 app.get("/", function(req, res){
@@ -37,7 +34,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function(req, res){
     const newUser = new User ({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     
     newUser.save(function(err){
@@ -50,7 +47,7 @@ app.post("/register", function(req, res){
 });
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, found){
         if (err) {
